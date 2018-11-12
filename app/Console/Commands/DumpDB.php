e44c0a32b7b820b9724f3a70de359931291af0bc
agungdh <agungdh@live.com>
2018-11-12 09:31:48 +0700

    update

---
+++ app/Console/Commands/DumpDB.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DumpDB extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dumpdb';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Dump the Database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->line("Some text");
+        $this->info("Hey, watch this !");
+        $this->comment("Just a comment passing by");
+        $this->question("Why did you do that?");
+        $this->error("Ops, that should not happen.");
+    }
+}
