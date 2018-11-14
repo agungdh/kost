47b9ece50c19636012f465d8584a5b7b0992c7cb
agungdh <agungdh@live.com>
2018-11-14 08:17:31 +0700

    update

--- resources/views/backend/user/profile.blade.php
+++ resources/views/backend/user/profile.blade.php
@@ -16,73 +16,32 @@
         <div class="body">
             {!! Form::model($user, ['route' => ['user.doProfile', $user->id], 'method' => 'put']) !!}
 
-                <label for="nama">Nama</label>
-                <div class="form-group">
-                    @php
-                    $class = $errors->has('nama') ? 'form-line error focused' : 'form-line';
-                    $message = $errors->has('nama') ? '<label class="error">' . $errors->first('nama') . '</label>' : '';
-                    @endphp
-                    <div class="{{ $class }}">
-                        {!!
-                            Form::text(
-                                'nama', 
-                                null, 
-                                [
-                                    'class'=> 'form-control', 
-                                    'id' => 'nama', 
-                                    'placeholder'=>'Nama',
-                                ])
-                        !!}
-                    </div>
-                    {!! $message !!}
-                </div>
-                
-                <label for="alamat">Alamat</label>
-                <div class="form-group">
-                    @php
-                    $class = $errors->has('alamat') ? 'form-line error focused' : 'form-line';
-                    $message = $errors->has('alamat') ? '<label class="error">' . $errors->first('alamat') . '</label>' : '';
-                    @endphp
-                    <div class="{{ $class }}">
-                        {!!
-                            Form::text(
-                                'alamat', 
-                                null, 
-                                [
-                                    'class'=> 'form-control', 
-                                    'id' => 'alamat', 
-                                    'placeholder'=>'Alamat',
-                                ])
-                        !!}
-                    </div>
-                    {!! $message !!}
-                </div>
-                
-                <label for="nohp">No HP</label>
-                <div class="form-group">
-                    @php
-                    $class = $errors->has('nohp') ? 'form-line error focused' : 'form-line';
-                    $message = $errors->has('nohp') ? '<label class="error">' . $errors->first('nohp') . '</label>' : '';
-                    @endphp
-                    <div class="{{ $class }}">
-                        {!!
-                            Form::text(
-                                'nohp', 
-                                null, 
-                                [
-                                    'class'=> 'form-control', 
-                                    'id' => 'nohp', 
-                                    'placeholder'=>'No HP',
-                                ])
-                        !!}
-                    </div>
-                    {!! $message !!}
-                </div>
+                {!!
+                    Form::adhText(
+                        'Nama',
+                        'nama'
+                    )
+                !!}
+
+                {!!
+                    Form::adhText(
+                        'Alamat',
+                        'alamat'
+                    )
+                !!}
+
+                {!!
+                    Form::adhText(
+                        'No HP',
+                        'nohp'
+                    )
+                !!}
 
                 {!!
                     Form::adhSelect2(
                         'Verifikasi No HP',
                         'verified_nohp', 
+                        true,
                         [
                             'y' => 'Terverifikasi',
                             'n' => 'Belum Terverifikasi',
@@ -96,6 +55,7 @@
                     Form::adhSelect2(
                         'Status',
                         'active', 
+                        true,
                         [
                             'y' => 'Aktif',
                             'n' => 'Belum Aktif',
@@ -112,12 +72,4 @@
         </div>
     </div>
 </div>
-@endsection
-
-@section('js')
-<script type="text/javascript">
-    $("#verified_nohp").select2();
-    $("#level").select2();
-    $("#active").select2();
-</script>
 @endsection
