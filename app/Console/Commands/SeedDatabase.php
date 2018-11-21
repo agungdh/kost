013e69cf245a3b73ff5102cf4e39cfd041b604e9
agungdh <agungdh@live.com>
2018-11-21 22:37:52 +0700

    update

--- app/Console/Commands/SeedDatabase.php
+++ app/Console/Commands/SeedDatabase.php
@@ -5,6 +5,12 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Hash;
 
+use App\User;
+use App\Kos;
+
+use agungdh\Pustaka;
+
+use DB;
 class SeedDatabase extends Command
 {
     /**
@@ -38,26 +44,62 @@ class SeedDatabase extends Command
      */
     public function handle()
     {
-        $faker = \Faker\Factory::create();
+        $faker = new \Faker\Generator();
+        $faker->addProvider(new \Faker\Provider\Base($faker));
+        $faker->addProvider(new \Faker\Provider\en_US\Text($faker));
+        $faker->addProvider(new \Faker\Provider\en_US\Person($faker));
+        $faker->addProvider(new \Faker\Provider\en_US\Address($faker));
+        $faker->addProvider(new \Faker\Provider\Internet($faker));
+        // User
+        // $users = [];
+        // for ($i=0; $i <= $this->argument('jumlahItem') - 1; $i++) { 
+        //    $users[$i]['email'] = $faker->email;
+        //    $users[$i]['password'] = Hash::make(1234);
+        //    $users[$i]['nama'] = $faker->name;
+        //    $users[$i]['alamat'] = $faker->address;
+        //    $users[$i]['nohp'] = '08' . $faker->numberBetween(1111111111, 9999999999);
+        //    $users[$i]['level'] = 'p';
+        //    $users[$i]['active'] = 'y';
+        //    $users[$i]['verified_nohp'] = $faker->randomElement(['y', 'n']);
+        // }
 
-        $users = [];
-        for ($i=0; $i <= $this->argument('jumlahItem') - 1; $i++) { 
-           $users[$i]['email'] = $faker->email;
-           $users[$i]['password'] = Hash::make(1234);
-           $users[$i]['nama'] = $faker->name;
-           $users[$i]['alamat'] = $faker->address;
-           $users[$i]['nohp'] = '08' . $faker->numberBetween(1111111111, 9999999999);
-           $users[$i]['level'] = 'p';
-           $users[$i]['active'] = 'y';
-           $users[$i]['verified_nohp'] = $faker->randomElement(['y', 'n']);
+        // User::insert($users);
+        // End User
+
+        // Kos
+        $kosses = [];
+        
+        $userId = [];
+        foreach (User::where('level', 'p')->get() as $value) {
+            $userId[] = $value->id;
         }
 
-        foreach ($users as $user) {
-            foreach ($user as $key => $value) {
-                $this->line($key . ' = ' . $value);
-            }
-            $this->line("\n");
+        $desaId = [];
+        foreach (DB::table('v_daerah')->where('prop_id', '18')->get() as $value) {
+            $desaId[] = $value->desa_id;
         }
+
+        for ($i=0; $i <= $this->argument('jumlahItem') - 1; $i++) {
+            $kosses[$i]['id_user'] = $faker->randomElement($userId);
+            $kosses[$i]['nama'] = $faker->name;
+            $kosses[$i]['alamat'] = $faker->address;
+            $kosses[$i]['id_desa'] = $faker->randomElement($desaId);
+            $kosses[$i]['tipe'] = $faker->randomElement(['l', 'p', 'lp']);
+            $kosses[$i]['bulanan'] = Pustaka::decimalRand(300000, 1000000, 100000);
+            $kosses[$i]['tahunan'] = Pustaka::decimalRand(1500000, 10000000, 250000);
+            $kosses[$i]['deskripsi'] = $faker->realText(500);
+            $kosses[$i]['kamartersedia'] = $faker->randomNumber(2);
+            $kosses[$i]['latitude'] = $faker->latitude();
+            $kosses[$i]['longitude'] = $faker->longitude();
+            $kosses[$i]['verified_alamat'] = $faker->randomElement(['y', 'n']);
+        }
+        Kos::insert($kosses);
+
+        // $fotos = [];
+        // foreach ($kosses as $kos) {
+            
+        // }
+        // End Kos
 
     }
 }
